654ab7ad1513ae84f84ee3b9779042d0bff52b70
dwissaaj <dwis.buntelman123@gmail.com>
2022-03-23 17:16:42 +0700

    Update req File

--- Supervised/Sentiment Analysis BERT/sentiment_classification.py
+++ Supervised/Sentiment Analysis BERT/sentiment_classification.py
@@ -2,4 +2,16 @@ import pandas as pd
 import plotly.express as px
 import plotly.io as pio
 pio.renderers.default = "browser"
+import os
+import shutil
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_hub as text
+from official.nlp import optimization  # to create AdamW optimizer
+
+import matplotlib.pyplot as plt
+df = pd.read_excel("data_training.xlsx")
+dfg = df.groupby('label').count()
+fig = px.bar(dfg,x="text",title='Distribution')
+
 
