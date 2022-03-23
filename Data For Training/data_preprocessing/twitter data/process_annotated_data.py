bb04df6cb751a3ea3fa595af56eb8ca4297581d8
dwisaji <dwis.buntelman123@gmail.com>
2022-03-23 14:59:22 +0700

    Refactor all function save to modul

--- Data For Training/data_preprocessing/twitter data/process_annotated_data.py
+++ Data For Training/data_preprocessing/twitter data/process_annotated_data.py
@@ -1,14 +1,5 @@
 import pandas as pd
-def load_json(file):
-    dataframe = pd.read_json(f'{file}',lines=True)
-    return dataframe
+from Modul.funcm import functions
 
-df1 = load_json("sen_data.jsonl")
-df2 = load_json("sentiment.jsonl")
-df3 = load_json("sentiment2.jsonl")
-df4 = load_json("sentiment3.jsonl")
-
-df1.to_excel("df1.xlsx")
-df2.to_excel("df2.xlsx")
-df3.to_excel("df3.xlsx")
-df4.to_excel("df4.xlsx")
+fc = functions
+df = fc.load_data("df.xlsx")
--- Unsupervised/lda_model.py
+++ Unsupervised/lda_model.py
@@ -1,14 +1,11 @@
 import re
 import warnings
-
 import gensim
 import gensim.models.ldamodel
 import matplotlib.pyplot as plt
-import pandas as pd
 import pyLDAvis
 from gensim import corpora
 from gensim.models import TfidfModel
-from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from pyLDAvis import gensim_models
 from wordcloud import WordCloud
@@ -16,63 +13,10 @@ from wordcloud import WordCloud
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
-def load_data(file):
-    """
-    Load_data fun reading excel file and change to ascii encoding for
-    removing the emoji in dataframe
-    """
-    data = pd.read_excel(file)
-    data = data.astype(str).apply(lambda x: x.str.encode('ascii', 'ignore').str.decode('ascii'))
-    return data
-
-
-def remove_username(dataframe, column: str):
-    """
-    Removing username in dataframe.You should this before use cleaning_data function
-    """
-    final = []
-    for text in dataframe[column].values.astype(str):
-        text = re.sub(r'@[\w]+', '', text)
-        final.append(text)
-        data = pd.DataFrame(final, columns=[column])
-    return data
-
-
-def cleaning_data(dataframe, column: str, hastag_return: bool):
-    """
-    Removing all character,symbol and etc in dataframe
-    """
-    hastag = dataframe[column].apply(lambda x: re.findall(r'\B#\w*[a-zA-Z]+\w*', x))
-    text = dataframe[column].str.replace(
-        r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', ' ', regex=True)
-    text = text.str.replace(r'#(\w+)', '', regex=True)
-    text = text.str.replace(r"[\"\'\|\?\=\.\\*\+\!\:\,]", '', regex=True)
-    text = text.str.replace(r'\d+', '', regex=True)
-    text = text.str.replace(r'RT', '', regex=True)
-    text = text.str.replace(r'\n', ' ', regex=True)
-    text = text.str.replace(r'\s\s+', " ", regex=True)
-    text = text.str.lstrip()
-    text = text.str.lower()
-    if hastag_return == True:
-        hastag = hastag.to_frame(name="hastag")
-        text = text.to_frame(name=column)
-        return pd.concat([text, hastag], axis=1)
-    else:
-        return text.to_frame(name=column)
-
-
-def remove_stopwords(dataframe, column: str, lang: str):
-    """
-    Removing stopwords inside dataframe. Need install NLTK Stopwords to specify stopword list
-    """
-    stop = stopwords.words({lang})
-    data = dataframe[column].apply(lambda x: ' '.join([word for word in x.split() if word not in stop]))
-    return data.to_frame(name=column)
-
-
 def generate_word(dataframe, column: str):
     """
-    Tokenize using NLTK for split each word
+    :param dataframe: Dataframe
+    :param column: Target Column
     """
     data = dataframe[column].apply(word_tokenize)
     return data
@@ -80,7 +24,7 @@ def generate_word(dataframe, column: str):
 
 def id_to_word(dataframe):
     """
-    Change dataframe into id to word
+    :param dataframe: Dataframe Target
     """
     idto_word = corpora.Dictionary(dataframe)
     return idto_word
@@ -88,7 +32,8 @@ def id_to_word(dataframe):
 
 def create_corpus(dataframe, column: str):
     """
-    Creating corpus by combine the id to word
+    :param dataframe: Dataframe
+    :param column: Target Column
     """
 
     generated = generate_word(dataframe, f"{column}")
@@ -102,7 +47,9 @@ def create_corpus(dataframe, column: str):
 
 def lda_model(corpuslda, id_two_w, topic: int):
     """
-    Calling LDA model gensim
+    :param corpuslda: Corpus Result
+    :param id_two_w: id2word Result
+    :param topic: Number of Topic
     """
     lda_mdl = gensim.models.ldamodel.LdaModel(corpus=corpuslda,
                                               id2word=id_two_w,
@@ -112,10 +59,16 @@ def lda_model(corpuslda, id_two_w, topic: int):
 
 
 def visualize(lda_data, corpus_list, id2word_data, mds: str, r: int,filename:str):
-    """
-    Visualize the LDA model using pyLDAVis
-    """
+    '''
+    :param lda_data: LDA Model Result
+    :param corpus_list: Corpus Result
+    :param id2word_data: id2word Result
+    :param mds: Default is "mmds"
+    :param r: Int Value
+    :param filename: File Name Output
+    :return:
 
+    '''
     vis = pyLDAvis.gensim_models.prepare(topic_model=lda_data,
                                          corpus=corpus_list,
                                          dictionary=id2word_data,
@@ -124,7 +77,7 @@ def visualize(lda_data, corpus_list, id2word_data, mds: str, r: int,filename:str
     pyLDAvis.save_html(vis,f"{filename}.html")
     return vis
 
-
+visualize()
 def remove_high_tfidf(dataframe, column: str, low_value: float):
     """
     USE THIS IF YOU HAVE A HIGH TFIDF WORD
@@ -160,11 +113,15 @@ def remove_high_tfidf(dataframe, column: str, low_value: float):
     return corpustfidf, id2w
 
 
-def to_json(filename: str, dataframe):
-    with open(f'{filename}.json', 'w') as f:
-        f.write(dataframe.to_json(orient="records"))
 
 def get_hastag(dataframe,column:str)-> list:
+    """
+
+    :param dataframe: Dataframe
+    :param column: Target Column
+    :return: List of Hastag
+    """
+
     nan_value = float("NaN")
     dataframe[column] = dataframe[column].apply(lambda x: re.findall(r'\B#\w*[a-zA-Z]+\w*', x))
     hastag = dataframe.astype(str).drop_duplicates()
@@ -178,6 +135,12 @@ def get_hastag(dataframe,column:str)-> list:
     return tagstr
 
 def wordcloud_maker(stack:list,colormap:str):
+    '''
+
+    :param stack: The list of the hastag
+    :param colormap: color map in matplotlib
+    :return
+    '''
     to_str = ' '.join(map(str,stack))
     wordcloud = WordCloud(width=2000, height=2000,max_words=75, colormap=f"{colormap}").generate(to_str)
     plt.figure()
@@ -187,7 +150,7 @@ def wordcloud_maker(stack:list,colormap:str):
     plt.show()
 
 
-
+"""
 df = load_data("Voxpopdata.xlsx")
 remove_user = remove_username(df,"text")
 clean_data = cleaning_data(remove_user,"text",False)
@@ -195,19 +158,19 @@ remove_stop = remove_stopwords(clean_data,"text","indonesian")
 toenize = generate_word(clean_data,"text")
 id2word = id_to_word(toenize)
 corpus = create_corpus(remove_stop,"text")
-
+vish = visualize(lda,corpus,id2word,"mmds",10,"normal")
 
 ldah = lda_model(corpus,id2word,25 )
 hastag = get_hastag(df,"text")
 wordcloud_maker(hastag,"Set2")
-
+"""
 
 """"
 lowcor = remove_high_tfidf(clean_data,"text",0.1)[0]
 lowid = remove_high_tfidf(clean_data,"text",0.1)[1]
 ldal = lda_model(lowcor,lowid,25 )
-viz2 = visualize(ldal,corpus,id2word,"mmds",1,"pemilul6")
+
 """
-wd = wordcloud_maker(hastag,"Set1")
-#vish = visualize(lda,corpus,id2word,"mmds",10,"normal")
+
+
 
